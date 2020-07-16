8d2c035cfc4af3f0aca354feacf2abc41cd14fcf
Sion Wilks-Boguszewicz <58896718+sioncamara@users.noreply.github.com>
2020-07-16 10:49:44 -0500

    added production mode

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+ app.use(express.static('client/build'));
 }
-
+ 
 // HTTP request logger
 app.use(morgan("tiny"));
 app.use("/api", apiRouts);
